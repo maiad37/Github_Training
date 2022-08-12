3e2697a3630ce3ead7fcffc9388f29d1c4eaf72c
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-08-12 17:40:08 -0300

    Update modifyTableDataTraining.rb

--- modifyTableDataTraining.rb
+++ modifyTableDataTraining.rb
@@ -3,7 +3,6 @@ client = Mysql2::Client.new(host: "localhost", username: "maia", password:"maia"
 data = client.query("Select id, lastname, email, email2 from people_maia").to_a
 
 def editTableTraining(data, client)
-
   data.each do |hash|
     newLastNames = hash["lastname"].gsub("'", "").to_s + " - EDITED"
     client.query("Update people_maia set lastname = '#{newLastNames}', email = '#{hash["email"].downcase.gsub("'", "")}', email2 = '#{hash["email2"].downcase.gsub("'", "")}' where id = #{hash["id"]};")
