05eb9bcabc550fca2c7dd90680331f91ce3555de
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-08-24 19:56:24 -0300

    updated

--- teacher_queries/db09_practice.rb
+++ teacher_queries/db09_practice.rb
@@ -1,11 +1,15 @@
 require 'mysql2'
 require 'dotenv/load'
 require_relative 'methods.rb'
+require 'digest'
 
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'],database: "applicant_tests")
 
 #get_teacher(2, client)
 #puts get_subject_teacher(client, 2)
 #puts get_class_subjects(client, "Class B")
-puts get_teachers_list_by_letter(client, "a")
+#puts get_teachers_list_by_letter(client, "a")
+#puts set_md5(client)
+#puts get_class_info(client, 1)
+puts get_teachers_by_year(client, 1970)
 client.close
--- teacher_queries/methods.rb
+++ teacher_queries/methods.rb
@@ -71,3 +71,48 @@ GROUP BY teachers_maia.first_name"
   end
   str
 end
+
+#4
+def set_md5(client)
+  r = "SELECT concat(first_name, middle_name, last_name, birth_date, current_age) AS con, teacher_id FROM teachers_maia"
+
+  teacher_md5 = client.query(r).to_a
+
+  teacher_md5.each do |row|
+    client.query("UPDATE teachers_maia SET md5 = \"#{Digest::MD5.hexdigest row["con"]}\" WHERE teacher_id = #{row["teacher_id"]}")
+  end
+  puts "success"
+end
+
+#5
+def get_class_info(client, id)
+    r = "SELECT classes_maia.name AS class, teachers_maia.first_name, teachers_maia.middle_name, teachers_maia.last_name FROM classes_maia JOIN teachers_maia ON classes_maia.responsible_teacher_id = teachers_maia.teacher_id WHERE classes_maia.classes_id = #{id};"
+    class_info = client.query(r).to_a
+    str = ""
+
+    if class_info.count.zero?
+      "No results"
+    else
+      str +="Class name: #{class_info[0]["class"]} \nResponsible teacher: #{class_info[0]["first_name"]} #{class_info[0]["middle_name"]} #{class_info[0]["last_name"]} \nInvolved teachers: "
+      class_info.each do |row|
+        str += "\n- #{row["first_name"]}"
+      end
+      str
+    end
+end
+
+#6
+def get_teachers_by_year(client, year)
+  r = "SELECT first_name, middle_name, last_name FROM teachers_maia WHERE YEAR(birth_date) = #{year}"
+  teachers_years = client.query(r).to_a
+  str = ""
+  if teachers_years.count.zero?
+    "No results"
+  else
+    str += "Teachers born in #{year}: "
+    teachers_years.each do |row|
+      str += "\n#{row["first_name"]} #{row["middle_name"]} #{row["last_name"]}"
+    end
+    str
+  end
+end
