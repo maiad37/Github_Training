e1d71c74e4a22f211fe9cf96653c3750d31e28e4
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-08-24 17:22:13 -0300

    Update db09_practice.rb

--- teacher_queries/db09_practice.rb
+++ teacher_queries/db09_practice.rb
@@ -5,7 +5,7 @@ require_relative 'methods.rb'
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'],database: "applicant_tests")
 
 #get_teacher(2, client)
-#get_subject_teacher(client, 1)
-#get_class_subjects(client, "Class A")
-get_teachers_list_by_letter(client, "A")
+#puts get_subject_teacher(client, 2)
+#puts get_class_subjects(client, "Class B")
+puts get_teachers_list_by_letter(client, "a")
 client.close
