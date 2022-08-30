7c8dc2f0c1d401067d2341dc5c7898b609d119de
Maia Dinsmore <69313479+maiad37@users.noreply.github.com>
2022-08-30 19:57:08 -0300

    cleaning_data

--- teacher_queries/db09_practice.rb
+++ teacher_queries/db09_practice.rb
@@ -1,30 +1,15 @@
 require 'mysql2'
 require 'dotenv/load'
-require_relative 'methods.rb'
 require 'digest'
 require 'date'
+require_relative 'methods.rb'
+require_relative 'cleaning.rb'
 
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'],database: "applicant_tests")
 
-#get_teacher(2, client)
-#puts get_subject_teacher(client, 2)
-#puts get_class_subjects(client, "Class B")
-#puts get_teachers_list_by_letter(client, "a")
-#puts set_md5(client)
-#puts get_class_info(client, 1)
-#puts get_teachers_by_year(client, 1970)
-#puts random_date(1980, 2003)
-#t = Time.now
-#100.times do
- # random_last_names(client, 1)
-#end
-#puts Time.now - t
 t = Time.now
-random_people(client, 10000)
+#random_people(client, 10000)
+clean_school_names (client)
 puts Time.now - t
-#100.times do
- # random_first_names(client, 1)
-#end
-#puts Time.now - t
 
 client.close
